717847d03337093555f98f055d5d9075a41ae6df
cristian <cristian.fonolla@gmail.com>
2017-03-20 11:58:44 +0100

    fixed task api

--- resources/assets/js/components/Todo.vue
+++ resources/assets/js/components/Todo.vue
@@ -1,18 +1,28 @@
-<style>
-
-</style>
-
-<template>
+<template xmlns:v-on="http://www.w3.org/1999/xhtml">
     <tr>
-        <td>{{index + from }}</td>
-        <td>
-            <template v-if="!editing">
-                <span @dblclick="edit">{{todo.name}}</span>
-            </template>
-            <input v-model="todo.name" v-show="editing" @keyup.esc="unedit" @keyup.enter="save"> <i class="fa fa-fw fa-edit" v-show="!editing" @click="edit"></i> <i class="fa fa-fw fa-check" @click="save" v-show="editing"></i> <i class="fa fa-fw fa-close" v-show="editing" @click="unedit"></i>
+        <td>{{index + from}}</td>
+        <td><span v-show="!editing" @dblclick="canviaNom(index,todo)">{{ todo.name }}</span>
+            <input type="text" v-model="todo.name" v-show="editing" @keyup.enter="canviaNom(index,todo)"
+                   v-todo-focus="editing" onfocus="this.select();"
+                   @keyup.esc="canceleditName(todo)">
+            <span @click="canviaNom(index,todo)"><i class="fa fa-fw fa-edit " v-show="!editing"></i></span>
+            <span @click="canviaNom(index,todo)"><i class="fa fa-fw fa-check bg-green" v-show="editing"></i></span>
+            <span @click="canceleditName(todo)"><i class="fa fa-fw fa-close bg-red" v-show="editing"></i></span>
         </td>
-        <td>{{todo.priority}}</td>
-        <td>{{todo.done}}</td>
+        <td><div v-show="!editingPri" @dblclick="canviaVisiPrioritat(index,todo)">{{ todo.priority }}</div>
+            <input type="text" v-model="todo.priority" v-show="editingPri" @keyup.enter="canviaVisiPrioritat(index,todo)"
+                   v-todo-focus="editingPri" onfocus="this.select();"
+                   @keyup.esc="canceleditPri(todo)"></td>
+        <td><span v-if="todo.done">
+                                <input type="checkbox"
+                                       class="icheckbox_square-green"
+                                       checked=""
+                                       @click="modificaDone(index,todo)">
+                            </span>
+            <span v-else>
+                                <input type="checkbox" class="icheckbox_square-green" @click="modificaDone(index,todo)">
+                            </span>
+        </td>
         <td>
             <div class="progress progress-xs">
                 <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
@@ -20,56 +30,107 @@
         </td>
         <td><span class="badge bg-red">55%</span></td>
         <td>
-            <span class="btn btn-md btn-info">
-                <i class="fa fa-fw fa-edit" @click="edittodo"></i>
+
+            <span class="btn btn-md btn-info" @click="edittodo(index,todo)">
+                <i class="fa fa-fw fa-edit" ></i>
             </span>
-            <span class="btn btn-md btn-danger">
-                <i class="fa fa-fw fa-trash" @click="deletetodo(index)"></i>
-            </span>
+            <button class="btn btn-md btn-warning" v-on:click=" deletetodo(index,todo.id)"><i class="fa fa-trash-o"></i></button>
+
         </td>
     </tr>
-</template>
 
+</template>
+<style>
+
+</style>
 <script>
 
-export default {
-    props: ['todo','index','from'],
+    export default {
+        props: ['todo','index','from'],
 
-    data() {
-        return {
-            editing: false
+        data() {
+            return {
+                editing: false,
+                editingPri: false,
+                bufferedTodoname: null,
+                bufferedTodopri: 0 ,
+            }
+        },
+        created() {
+            console.log('Component created');
+        },
+        methods:{
+            hola : function() {
+                console.log("Hola");
+
+            },
+            canviaNom: function(index, todo) {
+                this.bufferedTodoname = todo.name;
+                this.editing = !this.editing;
+                if (!this.editing) this.modificaNom(index,todo);
+
+            },
+            modificaNom: function(index,todo){
+                this.updateApi(todo);
+                console.log(todo);
+            },
+            updateApi: function (todo){
+            this.$http.put('/api/v1/task/' + todo.id,{
+                name : todo.name,
+                priority : todo.priority,
+                done : todo.done,
+             }).then((response) => {
+                console.log(response);
+
+            }, (response) => {
+                // error callback
+                sweetAlert("Oops...", "Something went wrong!", "error");
+                console.log(response);
+            });
+            },
+            canviaVisiPrioritat: function(index,todo) {
+                this.bufferedTodopri = todo.priority;
+                this.editingPri = !this.editingPri;
+                if (!this.editingPri) this.modificaPrioritat(index,todo);
+            },
+            modificaPrioritat: function(index,todo){
+                this.updateApi(todo);
+            },
+            modificaDone: function(index,todo){
+                todo.done = !todo.done;
+                this.updateApi(todo);
+            },
+            deletetodo: function(index,id){
+                this.$emit('todo-deleted', index, id);
+            },
+            canceleditName: function(todo){
+                todo.name = this.bufferedTodoname;
+                this.editing = false;
+            },
+            canceleditPri: function(todo){
+                todo.priority = this.bufferedTodopri;
+                this.editingPri = false;
+            },
+            edittodo: function (index,todo){
+                this.canviaVisiPrioritat(index,todo);
+                this.canviaNom(index,todo)
+            }
+        },
+        directives: {
+            'todo-focus': function (el, value) {
+              if (value) {
+                el.focus()
+              }
+            }
         }
-    },
-
-    created() {
-        console.log('Component todo created.');
-    },
-
-    methods: {
-        hello: function() {
-            console.log('Hello');
-        },
-
-        edit: function() {
-            this.editing = true;
-        },
-
-        unedit: function() {
-            this.editing = false;
-        },
-
-        save: function() {
-            this.editing = false;
-        },
-
-        edittodo: function() {
-            console.log('TODO editing');
-        },
-
-        deletetodo: function() {
-            console.log('TODO deleting');
-            this.$emit('todo-deleted', index);
-        },
     }
-}
+
+//    $(function () {
+//            $('input').iCheck({
+//                checkboxClass: 'icheckbox_square-green',
+//                radioClass: 'iradio_square-green',
+//                increaseArea: '20%' // optional
+//            });
+//        });
 </script>
+
